075bd58af360311aebd0fcca59a1d8c82c973378
nitpum <nitpum@outlook.com>
2023-10-31 23:32:45 +0700

    fix: Fix rate limit

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 type Embed struct {
 	Description string `json:"description"`
 }
+
+const min_wait_time_rate_limit = 2
 
 func main() {
 	if len(os.Args) < 2 {
@@ -52,6 +55,15 @@ func main() {
 
 	reader := bufio.NewReader(file)
 
+	logChannels := make(chan string, 5)
+
+	go func() {
+		for {
+			message := <-logChannels
+			postWebhook(webhookUrl, message)
+		}
+	}()
+
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
@@ -77,7 +89,9 @@ func main() {
 			break
 		}
 
-		go postWebhook(webhookUrl, line)
+		go func() {
+			logChannels <- line
+		}()
 		fmt.Printf("%s", string(line))
 	}
 	fmt.Println("End")
@@ -108,16 +122,31 @@ func postWebhook(url string, content string) {
 		log.Fatal(err)
 	}
 
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Print("Error can't post to webhook")
-	}
-	defer resp.Body.Close()
+	for {
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			fmt.Print("Error can't post to webhook")
+		}
+		defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Can't read response body")
-	}
+		if resp.StatusCode == http.StatusTooManyRequests {
+			waitTime := min_wait_time_rate_limit
+			if resp.Header.Get("X-RateLimit-Reset") != "" {
+				if rateLimit, err := strconv.ParseFloat(resp.Header.Get("X-RateLimit-Reset"), 32); err == nil {
+					waitTime = int(rateLimit)
+				}
+			}
+			time.Sleep(time.Duration(waitTime) * time.Second)
 
-	fmt.Println(string(body))
+			continue
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Can't read response body")
+		}
+
+		fmt.Println(string(body))
+		break
+	}
 }
